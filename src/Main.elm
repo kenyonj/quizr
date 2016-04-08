72c1ebfb687db944585e1fdd393f62de937e79d3
Justin Kenyon <kenyonj@gmail.com>
2016-04-08 13:59:53 -0400

    add forms

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,43 @@
 module Main (..) where
 
-import Html exposing (Html, text)
+import Html exposing (..)
+import Html.Attributes exposing (for, id, type', placeholder)
 
 main : Html
 main =
-  text "Hello world"
+  view
+
+
+view : Html
+view =
+  div
+    []
+    [ pageTitle
+    , question
+    ]
+
+
+pageTitle : Html
+pageTitle =
+  h1 [] [ text "QuizR" ]
+
+
+question : Html
+question =
+  section
+    []
+    [ dl
+        []
+        [ dt [] [ text "Category" ]
+        , dd [] [ text "Movie" ]
+        , dt [] [ text "Difficulty" ]
+        , dd [] [ text "Hard" ]
+        ]
+    , h2 [] [ text "What's the name of the first Star Wars movie?" ]
+    , form
+        []
+        [ label [ for "answer" ] [ text "What's your answer?" ]
+        , input [ type' "text", id "answer" ] []
+        , input [ type' "submit", placeholder "Submit your answer" ] []
+        ]
+    ]
